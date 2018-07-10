5d4c8c513b188454f853d78aa66678de1a4dfe70
Jackson Zavarella <jzavarella@ryerson.ca>
2018-07-10 13:19:27 -0400

    Added pageable response and many queries on courses

--- src/main/java/ca/jackzavarella/courses/api/controllers/InstitutionsController.java
+++ src/main/java/ca/jackzavarella/courses/api/controllers/InstitutionsController.java
@@ -2,10 +2,13 @@ package ca.jackzavarella.courses.api.controllers;
 
 import ca.jackzavarella.courses.api.models.Course;
 import ca.jackzavarella.courses.api.models.Institution;
+import ca.jackzavarella.courses.api.responses.PagedResponse;
 import ca.jackzavarella.courses.api.services.DatabaseService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashMap;
 
 /**
  * Created by Jack Zavarella on 7/5/2018. :)
@@ -15,13 +18,19 @@ import java.util.List;
 public class InstitutionsController {
     private final DatabaseService databaseService;
 
+    @Value("${paged.response.max-page-size}")
+    private int MAX_PAGE_SIZE;
+
     public InstitutionsController(DatabaseService databaseService) {
         this.databaseService = databaseService;
     }
 
     @GetMapping
-    public List<Institution> getInstitutions() {
-        return this.databaseService.fetchAllInstitutions();
+    public PagedResponse<Institution> getInstitutions(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "20") int pageSize) {
+        Page<Institution> page = this.databaseService.fetchAllInstitutions(pageNumber, pageSize);
+        return new PagedResponse<>(page.getContent(), page.getTotalElements(), page.getTotalPages(), page.getPageable().getPageNumber(), page.getPageable().getPageSize());
     }
 
     @GetMapping("/{institution}")
@@ -30,7 +39,41 @@ public class InstitutionsController {
     }
 
     @GetMapping("/{institution}/courses")
-    public List<Course> getCourses(@PathVariable String institution, @RequestParam(required = false) String academicYear, @RequestParam(required = false) String programLevel) {
-        return this.databaseService.fetchCoursesWithQueries(institution, academicYear, programLevel);
+    public PagedResponse<Course> getCourses(
+            @PathVariable String institution,
+            @RequestParam(required = false) String academicYear,
+            @RequestParam(defaultValue = "Undergraduate") String programLevel,
+            @RequestParam(defaultValue = "") String codeContains,
+            @RequestParam(defaultValue = "") String titleContains,
+            @RequestParam(defaultValue = "") String descriptionContains,
+            @RequestParam(defaultValue = "") String offeringsContains,
+            @RequestParam(defaultValue = "") String equatesContains,
+            @RequestParam(defaultValue = "") String prerequisitesContains,
+            @RequestParam(defaultValue = "") String corequisitesContains,
+            @RequestParam(defaultValue = "") String restrictionsContains,
+            @RequestParam(defaultValue = "") String departmentsContains,
+            @RequestParam(defaultValue = "") String semestersContains,
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "20") int pageSize) throws Exception {
+        if (academicYear == null) {
+            academicYear = this.databaseService.fetchLatestAcademicYear(institution);
+        }
+        if (pageSize > this.MAX_PAGE_SIZE) {
+            throw new Exception("pageSize must not exceed the max page size of " + this.MAX_PAGE_SIZE);
+        }
+        HashMap<String, String> queries = new HashMap<>();
+        queries.put("codeContains", codeContains);
+        queries.put("titleContains", titleContains);
+        queries.put("descriptionContains", descriptionContains);
+        queries.put("offeringsContains", offeringsContains);
+        queries.put("equatesContains", equatesContains);
+        queries.put("prerequisitesContains", prerequisitesContains);
+        queries.put("corequisitesContains", corequisitesContains);
+        queries.put("restrictionsContains", restrictionsContains);
+        queries.put("departmentsContains", departmentsContains);
+        queries.put("semestersContains", semestersContains);
+
+        Page<Course> coursePage = this.databaseService.fetchCoursesWithQueries(institution, academicYear, programLevel, pageNumber, pageSize, queries);
+        return new PagedResponse<>(coursePage.getContent(), coursePage.getTotalElements(), coursePage.getTotalPages(), coursePage.getPageable().getPageNumber(), coursePage.getPageable().getPageSize());
     }
 }
---
+++ src/main/java/ca/jackzavarella/courses/api/responses/PagedResponse.java
@@ -0,0 +1,61 @@
+package ca.jackzavarella.courses.api.responses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jack Zavarella on 7/10/2018. :)
+ */
+public class PagedResponse<T> {
+    private List<T> content = new ArrayList<>();
+    private long totalElements;
+    private int totalPages, pageNumber, pageSize;
+
+    public PagedResponse(List<T> content, long totalElements, int totalPages, int pageNumber, int pageSize) {
+        this.content = content;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}
